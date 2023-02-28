a0cbae3366bdd979d727ba7988c8213fabd7db22
nooruzbay <nooruzbay@gmail.com>
2023-02-28 15:37:01 +0600

    deleted the comment model and comment migration

--- source/school_rating/models.py
+++ source/school_rating/models.py
@@ -28,10 +28,6 @@ class School(models.Model):
 
 
 
-class Comment(models.Model):
-    text = models.TextField(max_length=400, null=True, blank=True, verbose_name='Comment')
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='user', on_delete=models.CASCADE, verbose_name='User')
-    school = models.ForeignKey("school_rating.School", related_name='comments', on_delete=models.CASCADE, verbose_name='School')
 
 class Rating(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False, verbose_name='Name')
