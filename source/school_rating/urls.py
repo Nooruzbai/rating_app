ccf688455ffb85472e29cde690cd3684cfc27567
nooruzbai <nooruzbay@gmail.com>
2023-04-18 08:51:43 +0600

    Changed CommentViews to Class Based Views

--- source/school_rating/urls.py
+++ source/school_rating/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from school_rating.views.comment_views import CommentView, CommentDetailView
+from school_rating.views.comment_views import CommentDetailView, CommentListView, CommentDeleteView, \
+    CommentUpdateView, CommentCreateView
 from school_rating.views.rating_views import RatingListView, RatingDetailView
 from school_rating.views.school_views import SchoolListView, SchoolDetailView, SchoolCreateView, SchoolDeleteView, \
     SchoolUpdateView
@@ -11,7 +12,7 @@ app_name = 'school_rating'
 
 urlpatterns = [
     path('schools/list/', SchoolListView.as_view(), name='schools_list_view'),
-    path("school/create/", SchoolCreateView.as_view(), name='school_create_view'),
+    path('school/create/', SchoolCreateView.as_view(), name='school_create_view'),
     path('school/detail/<int:pk>/', SchoolDetailView.as_view(), name='school_detail_view'),
     path('school/delete/<int:pk>/', SchoolDeleteView.as_view(), name='school_delete_view'),
     path('school/update/<int:pk>', SchoolUpdateView.as_view(), name='school_update_view'),
@@ -20,6 +21,9 @@ urlpatterns = [
     path('rating/detail/<int:pk>/', RatingDetailView.as_view(), name="rating_detail_view"),
 
 
-    path('comment/', CommentView.as_view(), name='comment_view'),
-    path('comment/<int:pk>', CommentDetailView.as_view(), name='comment_detail_view')
+    path('comments/list/', CommentListView.as_view(), name='comment_list_view'),
+    path('comment/create/', CommentCreateView.as_view(), name='comment_create'),
+    path('comment/detail/<int:pk>/', CommentDetailView.as_view(), name='comment_detail_view'),
+    path('comment/delete/<int:pk>/', CommentDeleteView.as_view(), name = 'comment_delete_view'),
+    path('comment/update/<int:pk>', CommentUpdateView.as_view(), name='comment_update_view'),
 ]
--- source/school_rating/views/comment_views.py
+++ source/school_rating/views/comment_views.py
@@ -1,73 +1,35 @@
-from django.http import Http404
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework import status
+from rest_framework.generics import ListAPIView, RetrieveAPIView, DestroyAPIView, UpdateAPIView, CreateAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from school_rating.models import Comment
 from school_rating.serializers.comment_serializers import CommentSerializer
 
 
-class CommentView(APIView):
+class CommentListView(ListAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly, ]
-
-    @swagger_auto_schema(
-        operation_summary="Adds a comment",
-        operation_description="You need to send the current user_id, school_id, text"
-    )
-    def post(self, request, *args, **kwargs):
-        serializer = CommentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    serializer_class = CommentSerializer
+    queryset = Comment.objects.all()
 
 
-
-class CommentDetailView(APIView):
+class CommentCreateView(CreateAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly, ]
+    serializer_class = CommentSerializer
 
-    def get_object(self, pk):
-        try:
-            return Comment.objects.get(pk=pk)
-        except Comment.DoesNotExist:
-            raise Http404
 
-    @swagger_auto_schema(
-        operation_summary="Gets the comment for detailed view.",
-    )
-    def get(self, request, pk, *args, **kwargs):
-        comment = self.get_object(pk)
-        serializer = CommentSerializer(comment)
-        return Response(serializer.data)
+class CommentDetailView(RetrieveAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
+    serializer_class = CommentSerializer
+    queryset = Comment.objects.all()
 
-    @swagger_auto_schema(
-        operation_summary="Edits the comment or if does not exist adds it.",
-    )
-    def put(self, request, pk, *args, **kwargs):
-        comment = self.get_object(pk)
-        serializer = CommentSerializer(comment, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def patch(self, request, pk):
-        comment = self.get_object(pk)
-        serializer = CommentSerializer(comment,
-                                      data=request.data,
-                                      partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class CommentUpdateView(UpdateAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
+    serializer_class = CommentSerializer
+    queryset = Comment.objects.all()
 
-    @swagger_auto_schema(
-        operation_summary="Deletes the comment.",
-    )
-    def delete(self, request, pk, *args, **kwargs):
-        comment = self.get_object(pk)
-        comment.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
+class CommentDeleteView(DestroyAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
+    serializer_class = CommentSerializer
+    queryset = Comment.objects.all()
