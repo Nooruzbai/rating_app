3d8538c2e176a36e39f1eb27dfb8e1faefbe12b4
nooruzbai <nooruzbay@gmail.com>
2023-07-15 15:58:19 +0600

    updated fixtures

--- source/rating/settings.py
+++ source/rating/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-errj+*fono5%$0fr%v%a6j$w&+n3j#(!+lmr521%g+4ux_zn@n'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
