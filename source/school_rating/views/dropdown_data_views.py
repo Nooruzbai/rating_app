7a98c2eeabb2a04760b9214e9003dcc374f03351
nooruzbai <nooruzbay@gmail.com>
2023-08-31 18:46:55 +0600

    changed the format output for school type, settings, cities

--- source/school_rating/views/dropdown_data_views.py
+++ source/school_rating/views/dropdown_data_views.py
@@ -6,42 +6,41 @@ from rest_framework.response import Response
 @api_view(['GET'])
 @swagger_auto_schema(operation_description="Receives all cities")
 def get_school_cities_view(request):
-        content = {
-            "0": "Not Assigned",
-            "1": "Bishkek",
-            "2": "Naryn",
-            "3": "Karakol",
-            "4": "Osh",
-            "5": "Jalal-Abad",
-            "6": "Batken",
-            "7": "Talas",
-        }
+        content = [
+            {"id": 0, "name": "Not Assigned"},
+            {"id": 1, "name": "Bishkek"},
+            {"id": 2, "name": "Naryn"},
+            {"id": 3, "name": "Karakol"},
+            {"id": 4, "name": "Osh"},
+            {"id": 5, "name": "Jalal-Abad"},
+            {"id": 6, "name": "Batken"},
+            {"id": 7, "name": "Talas"},
+            {"id": 8, "name": "Bishkek"}
+        ]
         return Response(content)
 
 
 @api_view(['GET'])
 @swagger_auto_schema(operation_description="Receives all settings")
 def get_school_setting_view(request):
-    content = {
-        "0": "Not Assigned",
-        "1": "City",
-        "2": "Suburb",
-        "3": "Village",
-    }
-
+    content = [
+        {"id": 0, "name": "Not Assigned"},
+        {"id": 1, "name": "City"},
+        {"id": 2, "name": "Suburb"},
+        {"id": 3, "name": "Village"},
+    ]
     return Response(content)
 
 
 @api_view(['GET'])
 @swagger_auto_schema(operation_description="Receives all types")
 def get_school_type_view(request):
-    content = {
-        "0": "Not Assigned",
-        "1": "Public",
-        "2": "Private",
-        "3": "Non Commercial"
-    }
-
+    content = [
+        {"id": 0, "name": "Not Assigned"},
+        {"id": 1, "name": "Public"},
+        {"id": 2, "name": "Private"},
+        {"id": 3, "name": "Non Commercial"},
+    ]
     return Response(content)
 
 
